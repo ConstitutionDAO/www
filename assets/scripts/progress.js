e1193bb7d80625892358c34d4df09d34fca7bf33
Marco <me@mar.co.de>
2021-11-18 04:00:44 +0100

    Add marks for progress bar

--- assets/scripts/progress.js
+++ assets/scripts/progress.js
@@ -12,12 +12,32 @@ function numberWithCommas(x) {
 (async function () {
   const { dollars, eth, ethUsdConversion } = await fetchData();
   const targetUSD = 69_420_000;
+  const milestones = [
+    [14_000_000, "Qualification Goal", true],
+    [30_000_000, "Competitive Goal", false],
+    [69_420_000, "nice.", true],
+  ];
+
+  milestones.forEach(([value, text, hide]) => {
+    const milestone = document.createElement("div");
+    milestone.innerHTML = `
+    <div class="tooltip ${hide ? "hide" : ""}">
+      <div>$${numberWithCommas(value)}</div>
+      <div class="label">${text}</div>
+    </div>
+    `;
+    milestone.className = "milestone";
+    milestone.style.left = `${(value / targetUSD) * 100}%`;
+    document.getElementById("bar").appendChild(milestone);
+  });
 
   const percentage = (dollars / targetUSD) * 100;
 
   document.getElementById("bar").style.width = `${percentage}%`;
 
-  document.getElementById("raisedUsd").textContent = numberWithCommas(parseInt(dollars).toFixed(0));
+  document.getElementById("raisedUsd").textContent = numberWithCommas(
+    parseInt(dollars).toFixed(0)
+  );
   document.getElementById("targetUsd").textContent =
     numberWithCommas(targetUSD);
 
@@ -27,5 +47,5 @@ function numberWithCommas(x) {
     (targetUSD / parseInt(ethUsdConversion)).toFixed(3)
   );
 
-  document.getElementById("percent").textContent = `${percentage.toFixed(0)}%`
+  document.getElementById("percent").textContent = `${percentage.toFixed(0)}%`;
 })();
