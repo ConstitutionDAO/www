8015f9b0aa6ae97562491c728503d522cfeae5a7
Tony <tony@tavour.com>
2021-11-17 20:25:30 -0600

    add milestone markers

--- assets/scripts/progress.js
+++ assets/scripts/progress.js
@@ -9,16 +9,20 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+const milestoneMillions = [14_000_000, 30_000_000];
+
 (async function () {
   const { dollars, eth, ethUsdConversion } = await fetchData();
-  const fiveMillionUnits = dollars / 5_000_000
-  const targetUSD = Math.ceil(fiveMillionUnits) * 5_000_000
+  const fiveMillionUnits = dollars / 5_000_000;
+  const targetUSD = Math.ceil(fiveMillionUnits) * 5_000_000;
 
   const percentage = (dollars / targetUSD) * 100;
 
   document.getElementById("bar").style.width = `${percentage}%`;
 
-  document.getElementById("raisedUsd").textContent = numberWithCommas(parseInt(dollars).toFixed(0));
+  document.getElementById("raisedUsd").textContent = numberWithCommas(
+    parseInt(dollars).toFixed(0)
+  );
   document.getElementById("targetUsd").textContent =
     numberWithCommas(targetUSD);
 
@@ -28,5 +32,23 @@ function numberWithCommas(x) {
     (targetUSD / parseInt(ethUsdConversion)).toFixed(3)
   );
 
-  document.getElementById("percent").textContent = `${percentage.toFixed(0)}%`
+  document.getElementById("percent").textContent = `${percentage.toFixed(0)}%`;
+
+  for (var i = 0; i < milestoneMillions.length; i++) {
+    console.log(targetUSD);
+    console.log(milestoneMillions[i]);
+    const percentage = milestoneMillions[i] / targetUSD;
+
+    const marker = document.createElement("div");
+    marker.className = "milestone";
+    marker.style.left = percentage * 100 + "%";
+
+    const label = document.createElement("div");
+    label.className = "milestone-label";
+    label.style.left = percentage * 100 + "%";
+    label.innerText = "Milestone " + (i + 1);
+
+    document.getElementById("progress").appendChild(marker);
+    document.getElementById("progress").appendChild(label);
+  }
 })();
